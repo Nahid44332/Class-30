bc14fc2e92ec6c10a182936d90473189dbe5e9a6
Nahid44332 <nahidhossen3278@gmail.com>
2025-01-26 00:47:20 -0800

    Javascript

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log ("Hello World");
+//    Addition
+    console.log ("Addition");
+    console.log ("10+20");
+   rahim =10;
+   korim =20;
+
+   console.log(rahim+korim);
+
+    // Subtraction
+    console.log ("Subtraction");
+    console.log ("100-30");
+    x =100;
+    y =30;
+
+    console.log (x-y);
+
+    // Multiplication
+    console.log ("Multiplication");
+    console.log ("500*10");
+    nusrat =500;
+    nahid =10;
+
+    console.log (nusrat*nahid);
+
+    // Division
+    console.log ("Division");
+    console.log ("500/5");
+    Nahid =500;
+    Nusrat =5;
+
+    console.log (Nahid/Nusrat);
